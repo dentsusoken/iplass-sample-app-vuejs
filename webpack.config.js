047fe0b5d964f82f9767e2ac0112b4d4147259a7
housh <xpeng.sirui@contract.isid.co.jp>
2019-01-07 15:31:57 +0900

    fix #3
    ・webpack-dev-server脆弱性による対応、3.1.14にバージョンアップする。
    ・それに関連する必要なパッケージも更新する。

--- webpack.config.js
+++ webpack.config.js
@@ -52,22 +52,18 @@ module.exports = {
   performance: {
     hints: false
   },
+  mode: 'development',
   devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
+  module.exports.mode = 'production'
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
       }
     }),
     new webpack.LoaderOptionsPlugin({
